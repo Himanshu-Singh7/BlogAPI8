a3e87f9ea439e5a4beff93f4f3130e0a234f6624
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-12-13 18:11:26 +0530

    Delete myblog8/myblog8 directory

--- myblog8/myblog8/src/main/java/com/myblog8/Repository/PostRepo.java
+++
@@ -1,9 +0,0 @@
-package com.myblog8.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.myblog8.Entity.Post;
-
-public interface PostRepo extends JpaRepository<Post, Long> {
-
-}
